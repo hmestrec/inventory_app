c0315a983198abaead57b6a6574928ea6aa1c848
Hector <hectormestre1234@gmail.com>
2024-11-13 16:43:40 -0600

    new

--- public/js/register.js
+++ public/js/register.js
@@ -1,35 +1,33 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const registerForm = document.getElementById('register-form');
+document.getElementById('register-form').addEventListener('submit', async (event) => {
+    event.preventDefault();
+
     const registerError = document.getElementById('register-error');
+    const registerForm = document.getElementById('register-form');
+    const companyName = document.getElementById('company-name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
 
-    registerForm.addEventListener('submit', async (e) => {
-        e.preventDefault();
+    // Check if fields are filled
+    if (!companyName || !email || !password) {
+        registerError.textContent = 'All fields are required';
+        return;
+    }
 
-        const email = document.getElementById('email').value.trim();
-        const password = document.getElementById('password').value.trim();
+    try {
+        const response = await fetch('https://sheltered-ocean-88352-000ba16da54d.herokuapp.com/register', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ companyName, email, password })
+        });
 
-        // Check if fields are filled
-        if (!email || !password) {
-            registerError.textContent = 'All fields are required';
-            return;
+        if (!response.ok) {
+            const responseData = await response.json();
+            throw new Error(responseData.message || 'Failed to register user');
         }
 
-        try {
-            const response = await fetch('https://sheltered-ocean-88352-000ba16da54d.herokuapp.com/register', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ email, password })
-            });
-
-            if (!response.ok) {
-                const responseData = await response.json();
-                throw new Error(responseData.message || 'Failed to register user');
-            }
-
-            registerError.textContent = 'User registered successfully!';
-            registerForm.reset();
-        } catch (error) {
-            registerError.textContent = `Error: ${error.message}`;
-        }
-    });
+        registerError.textContent = 'User registered successfully!';
+        registerForm.reset();
+    } catch (error) {
+        registerError.textContent = `Error: ${error.message}`;
+    }
 });
